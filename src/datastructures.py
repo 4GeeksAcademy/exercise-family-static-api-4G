38eb8116cd0626e8a4e9b885df3bb4974a747f38
César Rodríguez <cesaralejandrodriv@gmail.com>
2026-01-14 11:18:37 +0000

    first commit

--- src/datastructures.py
+++ src/datastructures.py
@@ -5,17 +5,26 @@ Update this file to implement the following already declared methods:
 - get_member: Should return a member from the self._members list
 """
 
+
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
         self._next_id = 1
-        self._members = [
+        self._members= [
             {
-                "id": self._generate_id(),
                 "first_name": "John",
-                "last_name": last_name,
                 "age": 33,
                 "lucky_numbers": [7, 13, 22]
+            },
+            {
+                "first_name": "Jane", 
+                "age": 35,
+                "lucky_numbers": [10, 14, 3]
+            },
+            {
+                "first_name": "Jimmy",
+                "age": 5,
+                "lucky_numbers": [1]
             }
         ]
 
@@ -26,19 +35,27 @@ class FamilyStructure:
         return generated_id
 
     def add_member(self, member):
-        ## You have to implement this method
-        ## Append the member to the list of _members
-        pass
+        new_member = {
+            "id": self._generate_id(),
+            "first_name": member["first_name"],
+            "last_name": self.last_name,
+            "age": member["age"],
+            "lucky_numbers": member.get("lucky_numbers", [])
+        }
+        self._members.append(new_member)
+        return new_member
 
     def delete_member(self, id):
-        ## You have to implement this method
-        ## Loop the list and delete the member with the given id
-        pass
+        for i, member in enumerate(self._members):
+            if member["id"] == id:
+                return self._members.pop(i)
+            return None
 
     def get_member(self, id):
-        ## You have to implement this method
-        ## Loop all the members and return the one with the given id
-        pass
+       for member in self._members:
+           if member ["id"] == id:
+               return member
+           return None
 
     # This method is done, it returns a list with all the family members
     def get_all_members(self):
